7be0f58fa0b8d0f529e8fb1af0c680213ab11c3d
ktmrose <ktm.rose@yahoo.com>
2021-04-23 14:13:16 -0600

    added debugging statement when tokens are sent

--- scripts/client.js
+++ scripts/client.js
@@ -331,7 +331,9 @@ connection = null;
 function sendTokens(accessToken, refreshToken) {
     if (accessToken !== "" && refreshToken !== "" && connection !== null) {
 
-        connection.send(JSON.stringify({"UserId" : userId, "Access_Token" : accessToken, "Refresh_Token" : refreshToken, "Client_ID" : clientId}))
+        const message = JSON.stringify({"UserId" : userId, "Access_Token" : accessToken, "Refresh_Token" : refreshToken, "Client_ID" : clientId})
+        connection.send(message)
+        console.log(message)
     }
 }
 
